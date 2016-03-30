3381fbc5d5e23906435115eb7653aa95ce9de910
Matt <mparman6@yahoo.com>
2016-03-30 16:59:56 -0400

    removed mail attempt

--- server.js
+++ server.js
@@ -18,41 +18,7 @@ app.get("/portfolio", function(req, res) {
   res.sendFile(process.cwd() + "/views/portfolio.html");
 });
 
-// Mail Code
 
-var nodemailer = require("nodemailer");
-
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: "mparman6",
-        pass: "andy0119"
-    }
-});
-/*------------------SMTP Over-----------------------------*/
-
-/*------------------Routing Started ------------------------*/
-
-app.get('/',function(req,res){
-    res.sendfile('index.html');
-});
-app.get('/send',function(req,res){
-    var mailOptions={
-        to : req.query.to,
-        subject : req.query.subject,
-        text : req.query.text
-    }
-    console.log(mailOptions);
-    smtpTransport.sendMail(mailOptions, function(error, response){
-     if(error){
-            console.log(error);
-        res.end("error");
-     }else{
-            console.log("Message sent: " + response.message);
-        res.end("sent");
-         }
-});
-});
 
 
 
